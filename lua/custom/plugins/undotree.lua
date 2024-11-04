52c7914607194f88909418837422fdac15e1e8b3
marrero171 <50031007+marrero171@users.noreply.github.com>
2024-11-03 21:29:36 -0400

    Undo saves locally

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -10,6 +10,19 @@ return {
       if vim.loop.os_uname().sysname == 'Windows_NT' then
         vim.g.undotree_DiffCommand = 'FC'
       end
+
+      if vim.fn.has 'persistent_undo' == 1 then
+        local target_path = vim.fn.expand '~/.undodir'
+
+        -- Create the directory if it does not exist
+        if vim.fn.isdirectory(target_path) == 0 then
+          vim.fn.mkdir(target_path, 'p', 0700)
+        end
+
+        -- Set the undo directory and enable persistent undo
+        vim.opt.undodir = target_path
+        vim.opt.undofile = true
+      end
     end,
   },
 }
