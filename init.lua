5c15cdead3a834ceda9c53f16e0bc28d2de92091
marrero171 <50031007+marrero171@users.noreply.github.com>
2024-11-06 22:03:12 -0400

    In case of VS code

--- init.lua
+++ init.lua
@@ -93,19 +93,24 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
--- [[ Setting options ]]
-require 'options'
+if vim.g.vscode then
+  -- VSCode extension
+else
+  -- ordinary Neovim
+  -- [[ Setting options ]]
+  require 'options'
 
--- [[ Basic Keymaps ]]
-require 'keymaps'
+  -- [[ Basic Keymaps ]]
+  require 'keymaps'
 
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+  -- [[ Install `lazy.nvim` plugin manager ]]
+  require 'lazy-bootstrap'
 
--- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+  -- [[ Configure and install plugins ]]
+  require 'lazy-plugins'
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+  -- The line beneath this is called `modeline`. See `:help modeline`
+  -- vim: ts=2 sts=2 sw=2 et
 
-require 'godotlsp'
+  require 'godotlsp'
+end
