6d0584e7e7c2fe3ccfd367c28a1c2b992894ba27
Francisco <50031007+marrero171@users.noreply.github.com>
2024-12-22 19:39:57 -0400

    Keymaps

--- lua/vscode-config/keymaps.lua
+++ lua/vscode-config/keymaps.lua
@@ -36,13 +36,11 @@ end
 -- General keymaps
 vscmap({ 'n', 'v' }, '<leader>t', 'workbench.action.terminal.toggleTerminal')
 vscmap({ 'n', 'v' }, '<leader>b', 'editor.debug.action.toggleBreakpoint')
-vscmap({ 'n', 'v' }, '<leader>d', 'editor.action.showHover')
 vscmap({ 'n', 'v' }, '<leader>q', 'editor.action.quickFix')
 vscmap({ 'n', 'v' }, '<leader>sp', 'workbench.actions.view.problems')
 vscmap({ 'n', 'v' }, '<leader>cn', 'notifications.clearAll')
-vscmap({ 'n', 'v' }, '<leader>ff', 'workbench.action.quickOpen')
 vscmap({ 'n', 'v' }, '<leader>cp', 'workbench.action.showCommands')
-vscmap({ 'n', 'v' }, '<leader>pr', 'code-runner.run')
+vscmap({ 'n', 'v' }, '<leader>cb', 'gitlens.toggleFileBlame')
 vscmap({ 'n', 'v' }, '<leader>fd', 'editor.action.formatDocument')
 
 vscmap({ 'n', 'v' }, '<leader>ws', 'workbench.action.gotoSymbol')
@@ -50,7 +48,5 @@ vscmap({ 'n', 'v' }, '<leader>ws', 'workbench.action.gotoSymbol')
 vscmap({ 'n', 'v' }, '<leader>sf', 'workbench.action.quickOpen')
 vscmap({ 'n', 'v' }, '<leader>sg', 'workbench.action.findInFiles')
 
--- Project manager keymaps
-vscmap({ 'n', 'v' }, '<leader>pa', 'projectManager.saveProject')
-vscmap({ 'n', 'v' }, '<leader>po', 'projectManager.listProjectsNewWindow')
-vscmap({ 'n', 'v' }, '<leader>pe', 'projectManager.editProjects')
+-- Debugging
+vscmap({ 'n', 'v' }, '<leader>d', 'workbench.debug.action.toggleRepl')
